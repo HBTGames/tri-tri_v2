5d7d0fca76f40542f0047efe338667c5df5acb78
Brian <1219607233@qq.com>
2017-07-03 15:34:07 +0800

    tuto all elements on

---
+++ tri-tri/TutorialViewController.swift
@@ -0,0 +1,79 @@
+//
+//  TutorialViewController.swift
+//  tri-tri
+//
+//  Created by mac on 2017-07-03.
+//  Copyright © 2017 mac. All rights reserved.
+//
+
+import UIKit
+
+class TutorialViewController: UIViewController {
+
+    @IBAction func exit(_ sender: Any) {
+        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let nextViewController = storyBoard.instantiateViewController(withIdentifier: "MenuViewController") as! MenuViewController
+        nextViewController.modalTransitionStyle = .crossDissolve
+        self.present(nextViewController, animated: true, completion: nil)
+        
+    }
+    @IBOutlet var mainScrollView: UIScrollView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let tuto_text = UIImageView()
+        tuto_text.image = UIImage(named:"tuto_text")
+        tuto_text.frame = CGRect(x:0, y:0, width: self.mainScrollView.frame.width, height: self.mainScrollView.frame.height)
+        
+        let tuto_case_1 = UIImageView()
+        tuto_case_1.image = UIImage(named:"tuto_case_1")
+        tuto_case_1.frame = CGRect(x:0, y:0, width: self.mainScrollView.frame.width, height: self.mainScrollView.frame.height)
+        
+        let tuto_case_2 = UIImageView()
+        tuto_case_2.image = UIImage(named:"tuto_case_2")
+        tuto_case_2.frame = CGRect(x:mainScrollView.frame.width, y:0, width: self.mainScrollView.frame.width, height: self.mainScrollView.frame.height)
+        
+        let tuto_case_3 = UIImageView()
+        tuto_case_3.image = UIImage(named:"tuto_case_3")
+        tuto_case_3.frame = CGRect(x:2*mainScrollView.frame.width, y:0, width: self.mainScrollView.frame.width, height: self.mainScrollView.frame.height)
+        
+        let tuto_case_4 = UIImageView()
+        tuto_case_4.image = UIImage(named:"tuto_case_4")
+        tuto_case_4.frame = CGRect(x:3*mainScrollView.frame.width, y:0, width: self.mainScrollView.frame.width, height: self.mainScrollView.frame.height)
+        
+        let tuto_reward = UIImageView()
+        tuto_reward.image = UIImage(named:"tuto_reward")
+        tuto_reward.frame = CGRect(x:4*mainScrollView.frame.width, y:0, width: self.mainScrollView.frame.width, height: self.mainScrollView.frame.height)
+        
+        let tuto_reward_text = UIImageView()
+        tuto_reward_text.image = UIImage(named:"tuto_reward_text")
+        tuto_reward_text.frame = CGRect(x:4*mainScrollView.frame.width, y:0, width: self.mainScrollView.frame.width, height: self.mainScrollView.frame.height)
+        
+        mainScrollView.contentSize.width = mainScrollView.frame.width * 5
+        mainScrollView.addSubview(tuto_case_1)
+        mainScrollView.addSubview(tuto_case_2)
+        mainScrollView.addSubview(tuto_case_3)
+        mainScrollView.addSubview(tuto_case_4)
+        mainScrollView.addSubview(tuto_reward)
+        mainScrollView.addSubview(tuto_reward_text)
+        mainScrollView.addSubview(tuto_text)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
