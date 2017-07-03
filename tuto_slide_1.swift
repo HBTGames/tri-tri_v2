54f47dddf2555f929fd59fb4717cd178605c7cd8
Brian <1219607233@qq.com>
2017-07-03 13:25:32 +0800

    tuto scroll page xib setup

---
+++ tuto_slide_1.swift
@@ -0,0 +1,21 @@
+//
+//  tuto_slide_1.swift
+//  tri-tri
+//
+//  Created by mac on 2017-07-03.
+//  Copyright © 2017 mac. All rights reserved.
+//
+
+import UIKit
+
+class tuto_slide_1: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ tuto_slide_3.swift
@@ -0,0 +1,21 @@
+//
+//  tuto_slide_3.swift
+//  tri-tri
+//
+//  Created by mac on 2017-07-03.
+//  Copyright © 2017 mac. All rights reserved.
+//
+
+import UIKit
+
+class tuto_slide_3: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
